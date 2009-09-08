d4aec284a632cef6d3951b11bb74c2530409ad66
subsume <subsume@65ff4c4c-c963-11dd-aa7b-9fdd872f0417>
2009-09-08 15:58:49 +0000

    added management command for importing, new syntax, etc

--- trunk/__init__.py
+++ trunk/__init__.py
@@ -24,7 +24,7 @@ import logging
 class ValidationError(Exception):
     "(Almost straight copy from django.forms)"
     def __init__(self, message):
-        # ValidationError can be passed any object that can 
+    # ValidationError can be passed any object that can 
 	# be printed (usually a string) or a list of objects. "
         if isinstance(message, list):
             self.messages = ([smart_unicode(msg) for msg in message])
@@ -40,24 +40,33 @@ class ValidationError(Exception):
         return repr(self.messages)
 
 class ImportBaseClass(object):
-	def _alpha_importation(self): # returns top importation fired
+	"""
+	Returns the topmost importation, which will be the last dataset 
+	imported since relations need to exist before it can
+	"""
+	def _alpha_importation(self):
+		topmost_level = None
 		current_level = self 
-		while True:
-			if hasattr(current_level,'parent') and current_level.parent:
+		while not topmost_level:
+			if current_level.parent:
 				current_level = current_level.parent
 			else:
-				if not hasattr(current_level,'master_variants'):
-					current_level.master_variants = {}
+				topmost_level = current_level
 
-				return current_level
-	
+		if not hasattr(topmost_level,'master_variants'):
+			topmost_level.master_variants = {}
+
+		return topmost_level
+
 	def _get_variants_for_model(self,model_class):
-		alpha = self._alpha_importation()
-		# Go to first importation and look for master variants of a particular class
-		if not model_class.__name__ in alpha.master_variants:
-			alpha.master_variants[model_class.__name__] = {}
+		key = model_class.__name__
 
-		return alpha.master_variants[model_class.__name__]
+		# Go to first importation and look for master variants of a particular class
+		alpha = self._alpha_importation()
+		if not key in alpha.master_variants:
+			alpha.master_variants[key] = {}
+
+		return alpha.master_variants[key]
 
 class Field(ImportBaseClass):
 	" Derived from django.forms.Field "
@@ -72,6 +81,7 @@ class Field(ImportBaseClass):
 		if value and prompt:
 			raise "Makes no sense to specify 'value' and then ask for prompt"
 
+		self.parent = None
 		self.map = map
 		self.test = False 
 		self.max_length = max_length
@@ -88,11 +98,12 @@ class Field(ImportBaseClass):
 	
 	def prep_work(self,master_field_name,parent):
 		" This is basically a second __init__ "
-		if not hasattr(self,'parent'):
+		if not self.parent:
 			self.master_field_name = master_field_name
 			self.parent = parent
 			self.master = self.parent.Meta.master._meta.get_field(master_field_name)
 			self.slave = None
+
 			if self.slave_field_name:
 				self.slave = self.parent.Meta.slave._meta.get_field(self.slave_field_name)
 
@@ -113,29 +124,31 @@ class Field(ImportBaseClass):
 	def _update_get_variants_for_model(self):
 		variants = {}
 		variants.update(self._get_variants_for_model(self.master.rel.to))
-		#This runs through the values for all of its possible slave
-		#values, prompting you to associate each as 'variants' to the master field.
-		#When this is done the conversion will loop through uninterrupted
+		"""
+		This runs through the values for all of its possible slave
+		values, prompting you to associate each as 'variants' to the master field.
+		When this is done the conversion will loop through uninterrupted
 
-		# Check the initial import for a master variant dict
-		# Use whole queryset here because clean_FOO override might need other fields in old QS.
-		
+		Check the initial import for a master variant dict
+		Use whole queryset here because clean_FOO override might need other fields in old QS.
+		"""
+
 		pdb.set_trace() # Need to copy. Can't be eating th whole QS
 		slave_iterable = self.parent.Meta.queryset.order_by(self.slave_field_name) 
-		if not hasattr(self.parent, 'clean_%s' % self.master_field_name):  
-			"Since clean_FOO may the whole record, not just one field's value.. get it"
+
+		"""
+		The user didn't provide a custom clean method a la def clean_FOO
+		"""
+		if not hasattr(self.parent, 'clean_%s' % self.master_field_name):
 			slave_iterable = slave_iterable.values_list(self.slave_field_name,flat=True).distinct()	
 
 		if self.map:
-			# If a map returns an appropriate object, use it!
 			for v in slave_iterable:
 				if v and not str(v).lower() in variants and \
 						isinstance(self.map[v],self.master.__class__):
 					variants[str(v).lower()] = self.map[v]
 
 		if hasattr(self,'custom_clean_function'):
-			# If a custom clean function returns an appropriate object
-			# then go ahead and use that.
 			for v in slave_iterable:
 				if v and not str(v).lower() in variants and \
 						isinstance(self.custom_clean_function(v),self.master.rel.to):
@@ -235,8 +248,8 @@ class Field(ImportBaseClass):
 			"""
 			Map was given, a la importer.Field('x',map={'0': False,'1': True}) 
 			"""
-			if value in self.map:
-				value = self.map[value]
+			if str(value) in self.map:
+				value = self.map[str(value)]
 
 		if hasattr(self,'custom_clean_function'):
 			"""
@@ -380,79 +393,122 @@ class BaseImport(ImportBaseClass):
 		for i in self.fields:
 			self.fields[i].prep_work(i,self)
 
-		if not parent:
-			# Remove master variant lists which aren't needed
-			# This should be expanded to clean up before the last conversion
-			
+		if not self.parent:
+			"""
+			Remove master variant lists which aren't needed
+			This should be expanded to clean up before the last conversion
+			"""
 			remove_keys = [i for i in self.master_variants.keys()]
-			for i in self.fields:
-				if hasattr('rel',i):
-					if i.rel.to.__class__.__name__ in remove_keys:
-						del remove_keys[i.rel.to.__class__.__name__]
+			for name, field in self.fields.iteritems():
+				if hasattr(field.master,'rel') and field.master.rel:
+					key = field.master.rel.to.__name__
+					remove_keys = [i for i in remove_keys if not i == key]
 
 			for i in remove_keys:
+				print "Deleting master variants for %s" % i
 				del self.master_variants[i]
 
 			self.do_import()
 
 	def do_import(self,verify_by=None,unique=False):
 		logging.debug("Importing %s" % self.__class__.__name__)
-		paginator = Paginator(self.Meta.queryset,1000)
-		check_variants = False
-		if self._get_variants_for_model(self.Meta.master): # Its possible we're importing the same model twice.
-			check_variants = True # If so, make sure to check before conducting any imports
 
+		"""
+		We've going to do an additional check for imports which have already been done
+		(say two different models link to a User's model).
+		"""
+		check_variants = False
+		if self._get_variants_for_model(self.Meta.master):
+			check_variants = True 
+
+		"""
+		Next, we're going to process the legacy records in chunks to make sure
+		we don't fill up memory
+		"""
+		paginator = Paginator(self.Meta.queryset,1000)
 		for i in range(0,paginator.num_pages):
 			for slave_record in paginator.page(i + 1).object_list:
-				if check_variants and str(slave_record.pk) in self._get_variants_for_model(self.Meta.master):
+				"""
+				Have we already processed this record in another loop? If so, skip!
+				"""
+				if check_variants and slave_record.pk in self._get_variants_for_model(self.Meta.master):
 					continue
 
-				sleep(.001) # This seems to yield massive performance improvements.
+				"""
+				A sleep here seems to increase performance immensely. Gives db a chance to catch up?
+				"""
+				sleep(.001)
 				new_model = None
-				if verify_by: #Won't fire without imported data
+
+				"""
+				If import has already been run and verify_by has been specified, try to
+				grab the record quickly
+				"""
+				if verify_by: 
 					new_model = self._soft_import(slave_record,verify_by)
 
+				"""
+				Regular import method. The long way
+				"""
 				if not new_model:
 					new_model = self._hard_import(slave_record,unique)
 
+				"""
+				If a record was created, and if this isn't the last phase of the import,
+				store the legacy and new pks for quick retrieval later.
+
+				Also, do post_save work if called for by the user
+				"""
 				if new_model: 
 					if self.parent:
 						self._get_variants_for_model(self.Meta.master).update({slave_record.pk: new_model.pk})
-				#self.dataset[str(slave_record.pk)] = new_model.pk
+
 					if hasattr(self,'post_save'):
 						self.post_save(slave_record,new_model)
 
 		print "Processed: %s, Created: %s" % (self.Meta.queryset.count(),self.created)
 		logging.debug("Processed: %s, Created: %s" % (self.Meta.queryset.count(),self.created))
-		#print "Here's a shell to inspect what was created, or not"
-		#import pdb;pdb.set_trace()
 		self.created = 0 
 
+	"""
+	Used to match a table only by one field
+	Obviously this *NEVER* fires unless imported data exists.
+
+	This gives the user a quick way to grab models already imported
+	"""
 	def _soft_import(self,slave_record,verify_by):
-		#Used to match a table only by one field
-		#Obviously this *NEVER* fires unless imported data exists
 
+		"""
+		They may pass in a tuple which gives the correspondence.
+
+		If the field names are the same in master/slave, just pass one
+		"""
 		m_field = verify_by[0]
-
 		try:
 			s_field = verify_by[1] # Tuple, (m,s)
 			raise
 		except:
 			s_field = m_field # One field name for both 
-		
+	
 		verify_kwarg = {m_field: getattr(slave_record,s_field)}
-
 		try:
 			return self.Meta.master.objects.get(**verify_kwarg)
 		except self.Meta.master.DoesNotExist:
 			return
 
 	def _hard_import(self,slave_record,unique):
-		#Regular import. Matches two tables by all fields.
+		"""
+		Regular import method. Goes the long way to construct a record from
+		legacy data
+		"""
 		cleaned_data = {}
 		for field_name, field in self.fields.iteritems():
 			value = None
-			if field.slave_field_name: # Field may not exist in slave table 
+
+			"""
+			We don't assume there was any slave field.
+			"""
+			if field.slave_field_name:
 				value = getattr(slave_record,field.slave_field_name)
 
 			if field.importation:
@@ -470,6 +526,11 @@ class BaseImport(ImportBaseClass):
 
 				logging.debug("Grabbing related, ended up with %s" % value)
 
+			"""
+			Maybe a value was hard-passed into the field. Just use it.
+
+			Otherwise, pass it through the built-in quick and dirty clean methods
+			"""
 			if field_name in self.override_values and value in self.override_values[field_name]:
 				value = self.override_values[field_name][value]
 			else:
@@ -477,16 +538,25 @@ class BaseImport(ImportBaseClass):
 
 			cleaned_data[field_name] = value
 
-			if hasattr(self, 'clean_%s' % field_name): # use custom clean_FOO override
+			"""
+			If the user specified a custom clean_FOO, call it now
+			"""
+			if hasattr(self, 'clean_%s' % field_name): 
 				cleaned_data[field_name] = getattr(self,'clean_%s' % field_name)(slave_record,cleaned_data)
 
+		"""
+		If they specified a general clean function, call it now
+		"""
 		if hasattr(self,'clean'):
 			cleaned_data = self.clean(slave_record,cleaned_data)
 
-		if not cleaned_data: # There was nothing to create!
-			return
+		"""
+		If we didn't end up with data (probably return None in the clean() method),
+		let's not try to import anything. No record created. Otherwise, import!
+		"""
+		if cleaned_data:
+			return self._hard_import_commit(cleaned_data,unique)
 
-		return self._hard_import_commit(cleaned_data,unique)
 
 	def _hard_import_commit(self,cleaned_data,unique):
 		try:
---
+++ trunk/management/commands/import.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from optparse import make_option
+
+class Command(BaseCommand):
+	args = ['conversion class...']
+
+	def handle(self, conversion_class, **options):
+		imports = __import__('conversion/imports')
+		conversion = getattr(imports,conversion_class)
+		conversion()
